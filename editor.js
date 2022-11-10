1c7945ba2343879960ce71324463f18f3b9e8028
Saglai <power1800w@gmail.com>
2022-11-10 22:28:48 +0700

    refactor

--- editor.js
+++ editor.js
@@ -1,5 +1,4 @@
 'use strict'
-let pos1;
 
 let canvas = document.getElementById('canvas'); 
 let ctx = canvas.getContext('2d');
@@ -7,16 +6,20 @@ let lineButton = document.getElementById('line');
 let circleButton = document.getElementById('circle');
 
 // Events
-canvas.addEventListener('mousedown', lineDraw1);
-canvas.addEventListener('mouseup', lineDraw2);
+canvas.addEventListener('mousedown', lineMouseDownHandler);
+canvas.addEventListener('mouseup', lineMouseUpHandler);
+//canvas.addEventListener('mousemove', lineMouseMoveHandler);
+//canvas.addEventListener('mousemove', circleMouseMoveHandler);
 
 circleButton.addEventListener('click', removeLine);
 lineButton.addEventListener('click', removeCircle);
 
+
+// Functions
+
 // Get mousePosition
 function getMousePos(canvas, event) { 
     let rect = canvas.getBoundingClientRect(); 
-
     let x = event.clientX - rect.left;
     let y = event.clientY - rect.top;
     return [x, y]
@@ -48,52 +51,70 @@ function lineMouseMoveHandler(event) {
 
     let xy = getMousePos(canvas, event);
     ctx.beginPath();
-    ctx.lineTo(arr[0], arr[1]);
-}
+    ctx.moveTo(downX, downY);
+    ctx.lineTo(xy[0], xy[1]);
 
-function lineDraw2(event) { 
-    let arr = getMousePos(canvas, event);
-    ctx.lineTo(arr[0], arr[1]);
     ctx.lineWidth = 2;
     ctx.strokeStyle = 'black';
     ctx.stroke();
+
 }
 
+
 // Draw a circle
-function circleDraw1(event) {
+let circleMouseDown;
+let isMouseDownCircle = false;
+
+function circleMouseDownHandler(event) {
     let pos = getMousePos(canvas, event);
-    pos1 = pos;
+    circleMouseDown = pos;
+    isMouseDownCircle = true;
     ctx.beginPath();
 }
 
-function circleDraw2(event) {
-    let pos2 = getMousePos(canvas, event);
-    let avrX = (Math.abs(pos1[0] - pos2[0]) / 2) + Math.min(pos1[0], pos2[0]);
-    let avrY = (Math.abs(pos1[1] - pos2[1]) / 2) + Math.min(pos1[1], pos2[1]);
-    let diameter = Math.max( Math.abs(pos1[0] - pos2[0]), Math.abs(pos1[1] - pos2[1]) );
+function circleMouseUpHandler(event) {
 
+    isMouseDownCircle = false;
+}
+
+function circleMouseMoveHandler(event) {
+
+    if(!isMouseDownCircle) {
+        return
+    }
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    let circleMouseUp = getMousePos(canvas, event);
+    let avrX = (Math.abs(circleMouseDown[0] - circleMouseUp[0]) / 2) + Math.min(circleMouseDown[0], circleMouseUp[0]);
+    let avrY = (Math.abs(circleMouseDown[1] - circleMouseUp[1]) / 2) + Math.min(circleMouseDown[1], circleMouseUp[1]);
+    let diameter = Math.max( Math.abs(circleMouseDown[0] - circleMouseUp[0]), Math.abs(circleMouseDown[1] - circleMouseUp[1]) );
+
+    //ctx.beginPath();
     ctx.arc(avrX, avrY, diameter/2, 0, 2 * Math.PI);
     ctx.lineWidth = 2;
     ctx.strokeStyle = 'black';
     ctx.stroke();
-}
 
+}
 //Remove functions
 
 function removeLine() {
-    canvas.removeEventListener('mousedown', lineDraw1);
-    canvas.removeEventListener('mouseup', lineDraw2);
+    canvas.removeEventListener('mousedown', lineMouseDownHandler);
+    canvas.removeEventListener('mouseup', lineMouseUpHandler);
 
-    canvas.addEventListener('mousedown', circleDraw1);
-    canvas.addEventListener('mouseup', circleDraw2);
+    canvas.addEventListener('mousedown', circleMouseDownHandler);
+    canvas.addEventListener('mouseup', circleMouseUpHandler);
+    canvas.addEventListener('mousemove', circleMouseMoveHandler);
+
 }
 
 function removeCircle() {
-    canvas.removeEventListener('mousedown', circleDraw1);
-    canvas.removeEventListener('mouseup', circleDraw2);
+    canvas.removeEventListener('mousedown', circleMouseDownHandler);
+    canvas.removeEventListener('mouseup', circleMouseUpHandler);
 
-    canvas.addEventListener('mousedown', lineDraw1);
-    canvas.addEventListener('mouseup', lineDraw2);
+    canvas.addEventListener('mousedown', lineMouseDownHandler);
+    canvas.addEventListener('mouseup', lineMouseUpHandler);
 }
 
 
